7acff10bb05d2e099ef2f5e36c3e0d9f8ba24ab8
Guillian Aufrère <guillian77270@gmail.com>
2021-06-17 10:21:49 +0200

    Implement cache system

--- src/AfpaConnect.php
+++ src/AfpaConnect.php
@@ -4,8 +4,13 @@
 namespace Guillian\AfpaConnect;
 
 
+use Cache\Adapter\Filesystem\FilesystemCachePool;
 use Firebase\JWT\JWT;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem;
+use Psr\Cache\InvalidArgumentException;
 
 class AfpaConnect
 {
@@ -19,32 +24,73 @@ class AfpaConnect
 
     private ?string $jwt = null;
 
-    public function __construct($hostname, $issuer, $publicKey)
+    private FilesystemCachePool $cache;
+
+    public function __construct(string $hostname, string $issuer, string $publicKey)
     {
+        /**
+         * Configure cache system
+         */
+        $filesystemAdapter = new Local(dirname(__DIR__));
+        $filesystem = new Filesystem($filesystemAdapter);
+        $this->cache = new FilesystemCachePool($filesystem, "cache");
+
+        /**
+         * Initialize HTTP Client
+         */
         $this->client = new Client();
 
+        /**
+         * Initialize general configuration
+         */
         $this->hostname = trim($hostname, "/")."/api/";
-
         $this->issuer = $issuer;
-
         $this->publicKey = $publicKey;
 
+        /**
+         * Token handler
+         */
         $this->tokenHandler();
     }
 
+    /**
+     * Handle JWT.
+     *
+     * Check if JWT is present in cache pool.
+     * Check if JWT is expired.
+     *
+     * @throws InvalidArgumentException
+     */
     private function tokenHandler()
     {
-        if (empty($_SESSION['jsonWebToken'])) {
-            echo "------ TOKEN NOT CACHED ------\n";
-            $_SESSION['jsonWebToken'] = $this->getJsonWenToken();
+        $cachedJWT = $this->cache->getItem('jwt');
+
+        if (!$cachedJWT->isHit()) { // Check JWT presence.
+            $this->cacheJsonWebToken($cachedJWT);
         }
 
-        if ($this->isTokenExpired($_SESSION['jsonWebToken'])) {
-            echo "------ TOKEN EXPIRED ------\n";
-            $_SESSION['jsonWebToken'] = $this->getJsonWenToken();
+        if ($this->isTokenExpired($cachedJWT->get())) { // Check JWT expired.
+            $this->cacheJsonWebToken($cachedJWT);
         }
 
-        $this->jwt = $_SESSION['jsonWebToken'];
+        // Save JWT
+        $this->jwt = $cachedJWT->get();
+    }
+
+    /**
+     * Store JWT come from API into cache pool.
+     *
+     * @param $item
+     */
+    private function cacheJsonWebToken($item)
+    {
+        $jwt = $this->getJsonWenToken();
+
+        $item
+            ->set($jwt)
+            ->expiresAfter($this->decodeJWT($jwt)->exp);
+
+        $this->cache->save($item);
     }
 
     /**
@@ -52,7 +98,6 @@ class AfpaConnect
      */
     private function getJsonWenToken()
     {
-        echo "------ GET TOKEN ------\n";
         $respContent = $this->post(
             "auth",
             ['public_key' => $this->publicKey]
@@ -68,15 +113,21 @@ class AfpaConnect
      */
     private function isTokenExpired($jwt):bool
     {
-        $jwt = $this->decodeJWT($jwt);
-
-        if ($jwt->exp <= time()) {
+        try {
+            $this->decodeJWT($jwt);
+        } catch (\Exception $exception) {
             return true;
         }
 
         return false;
     }
 
+    /**
+     * Decode JWT.
+     *
+     * @param $jwt JWT to decode.
+     * @return object|null
+     */
     private function decodeJWT($jwt)
     {
         return JWT::decode($jwt, $this->publicKey, [
@@ -85,6 +136,18 @@ class AfpaConnect
         ]);
     }
 
+    /**
+     * Send POST request to AfpaConnect.
+     *
+     * Request is also send with issuer and JWT.
+     *
+     * @param string $route
+     * @param array $parameters
+     *
+     * @return string
+     *
+     * @throws GuzzleException
+     */
     public function post(string $route, array $parameters = [])
     {
         $url = $this->hostname . $route;
@@ -104,6 +167,18 @@ class AfpaConnect
         return $resp->getBody()->getContents();
     }
 
+    /**
+     * Send GET request to AfpaConnect.
+     *
+     * Request is also send with issuer and JWT.
+     *
+     * @param string $route
+     * @param array $parameters
+     *
+     * @return string
+     *
+     * @throws GuzzleException
+     */
     public function get(string $route, array $parameters = [])
     {
         $url = $this->hostname . $route;
@@ -114,7 +189,10 @@ class AfpaConnect
         );
 
         $resp = $this->client->request('GET', $url, [
-            "form_params" => $parameters
+            "query" => $parameters,
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->jwt
+            ]
         ]);
 
         return $resp->getBody()->getContents();
--- test.php
+++ test.php
@@ -4,12 +4,25 @@ use Guillian\AfpaConnect\AfpaConnect;
 
 require 'vendor/autoload.php';
 
+/**
+ * Debug.
+ * @param $toDebug
+ */
+function dd($toDebug) {
+    echo "<pre>";
+    var_dump($toDebug);
+    echo "</pre>";
+}
+
 session_start();
 
 $publicKey = file_get_contents("tests/afpanier.key");
 
 $ac = new AfpaConnect("http://localhost/AfpaConnect/", "afpanier", $publicKey);
 
+/**
+ * POST EXAMPLE
+ */
 $resp = $ac->post("register", [
     'username' => "123456789",
     'password' => "test"
@@ -17,6 +30,15 @@ $resp = $ac->post("register", [
 
 $resp = json_decode($resp);
 
-echo "<pre>";
-var_dump($resp);
-echo "</pre>";
+dd($resp);
+
+/**
+ * GET EXAMPLE
+ */
+$resp = $ac->get("user", [
+    'username' => "123456789"
+]);
+
+$resp = json_decode($resp);
+
+dd($resp);
